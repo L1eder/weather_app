2532c1749ed9b3d71f75a3840e6f33abc665d3d8
Lydia <lida_zhuikova@mail.ru>
2025-05-11 17:43:44 +0400

    chore: changed the project structure, changed the location of the ui files

--- app/src/main/java/com/example/weatherapp/data/models/WeatherResponse.kt
+++ app/src/main/java/com/example/weatherapp/data/models/WeatherResponse.kt
@@ -4,19 +4,3 @@ data class WeatherResponse(
     val daily: DailyForecast,
     val hourly: HourlyForecast
 )
-
-data class DailyForecast(
-    val time: List<String>,
-    val temperature_2m_max: List<Double>,
-    val temperature_2m_min: List<Double>,
-    val precipitation_sum: List<Double>,
-    val wind_speed_10m_max: List<Double>
-)
-
-data class HourlyForecast(
-    val time: List<String>,
-    val temperature_2m: List<Double>,
-    val precipitation: List<Double>,
-    val wind_speed_10m: List<Double>,
-    val weather_code: List<Int>
-)
